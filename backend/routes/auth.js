420bed71bdbe727d068bcdc8b7cdd38644ab7911
IshikaB3003 <ishikabansal3003@gmail.com>
2022-11-30 22:47:10 +0530

    express validation done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/User')
 const router = express.Router();
 
@@ -9,12 +10,28 @@ const router = express.Router();
     //     number: 45
     // }
     // res.json(obj)
-    router.post('/',(req,res)=>{
-        console.log(req.body);
-        const user = User(req.body);
-        user.save()
-        res.send(req.body);
+    router.post('/',[
+        body('email').isEmail(),
+        body('password').isLength({ min: 5 }),
+        body('name','enter at least 3 chars').isLength({ min: 3 }),
+    ],(req,res)=>{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+          }).then(user => res.json(user))
+          .catch(err=>{console.log(err)
+            res.json({error:'Please enter a unique value for email',message:err.message})})
+
+        
+
     })
 
 
 module.exports = router
+
+// res.send(req.body);
